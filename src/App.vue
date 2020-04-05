a832d19355bf2d1a96922c3e598d6ef456199c07
paweemew <60011252@kmitl.ac.th>
2020-04-05 17:18:51 +0700

    mvp version

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to eiei Your Vue.js App"/>
+  <div>
+    <h1>{{Title}}</h1>
+    <button>Dashboard</button>
+    <button>Data History</button>
+    <h2>Temprature: 20°</h2>
+    <h2>Humidity: 55%</h2>
+    <h2>Oxygen:150 Mg</h2>
+    <h2>PH: 6.4</h2>
+    <h2>EC: 3</h2>
+  <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name:"app",
+  data() {
+    return {
+      Title:"Bally Smartfarm",
+    }
+  },
+
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
+
+
+
+
