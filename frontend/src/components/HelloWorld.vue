1399f09d65103de5332a74f904472e5ade3d6759
Woraphot Khamthabthim <60011288@kmitl.ac.th>
2020-04-26 18:40:35 +0700

    Visualize data in line chart

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,8 +1,9 @@
 <template>
 <div>
-  <chartjs-doughnut v-bind:labels="labels"
-    v-bind:datasets="datasets"
-    v-bind:option="option"></chartjs-doughnut>
+  <chartjs-line v-bind:beginzero="beginZero"
+    v-bind:datalabel="datalabel" v-bind:labels="labels"
+    v-bind:data="data" v-bind:fill="fill"
+    v-bind:bodercolor="boderColor"/>
 </div>
 </template>
 
@@ -10,21 +11,16 @@
 export default {
   data() {
     return {
-      labels:["good","bad"],
-      datasets: [
-        {
-          data: [50,50],
-          backgroundColor: ["Green", "Red"]
-        }
-      ],
-      option:{
-        title: {
-          display: true,
-          position: "bottom",
-          text: "Temperature"
-        }
-      }
-    };
+      beginZero: true,
+      datalabel: "Temperature",
+      labels: [1.00, 2.00, 3.00, 4.00, 5.00, 6.00, 7.00, 8.00, 9.00, 10.00, 11.00, 12.00, 13.00, 14.00, 15.00, 16.00, 17.00, 18.00, 19.00, 20.00, 21.00, 22.00, 23.00, 0.00],
+      data: [19, 19, 18, 18, 19, 20, 20, 20, 21, 21, 25, 27, 28, 28, 28, 27, 26, 25, 24, 24, 24, 22, 21, 20],
+      fill: true,
+      bgColor: "pink",
+      borderColor: "	#FF0000"
+      
+      
+    }
   }
 };
 </script>
